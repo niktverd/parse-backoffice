b965cbef920715f1ada42a9fd5e581a318bd59e3
Nik Tverd <nn.tverd@gmail.com>
2024-01-24 11:09:55 +0600

    fix: secret obtaining

--- configs/auth.ts
+++ configs/auth.ts
@@ -1,16 +1,19 @@
 import {FirestoreAdapter as firestoreAdapter} from '@auth/firebase-adapter';
 import {cert} from 'firebase-admin/app';
+import yaml from 'js-yaml';
 import {AuthOptions} from 'next-auth';
 // import CredentialsProvider from 'next-auth/providers/credentials';
 import github from 'next-auth/providers/github';
 import googleProvider from 'next-auth/providers/google';
+
+const firebasePrivateKey = yaml.load(process.env.FIREBASE_PRIVATE_KEY || 'value: ""') as {value: string};
 
 export const authConfig: AuthOptions = {
     adapter: firestoreAdapter({
         credential: cert({
             projectId: process.env.FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY,
+            privateKey: firebasePrivateKey.value,
         }),
     }),
     providers: [
