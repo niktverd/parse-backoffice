da1c9c0d2db8142ba481120adfa18708d3f3ab52
Nik Tverd <nn.tverd@gmail.com>
2024-01-24 11:31:50 +0600

    fix: another secret attempt

--- configs/auth.ts
+++ configs/auth.ts
@@ -1,21 +1,15 @@
 import {FirestoreAdapter as firestoreAdapter} from '@auth/firebase-adapter';
 import {cert} from 'firebase-admin/app';
-import yaml from 'js-yaml';
 import {AuthOptions} from 'next-auth';
-// import CredentialsProvider from 'next-auth/providers/credentials';
 import github from 'next-auth/providers/github';
 import googleProvider from 'next-auth/providers/google';
-
-const firebasePrivateKey = yaml.load(process.env.FIREBASE_PRIVATE_KEY || 'value: ""') as {value: string};
-// eslint-disable-next-line no-console
-console.log(firebasePrivateKey);
 
 export const authConfig: AuthOptions = {
     adapter: firestoreAdapter({
         credential: cert({
             projectId: process.env.FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: firebasePrivateKey.value,
+            privateKey: process.env.FIREBASE_PRIVATE_KEY,
         }),
     }),
     providers: [
